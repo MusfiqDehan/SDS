a41018ad20c7d2f8102ccf25183a1ff1068ee7b6
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-12-19 02:01:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.querySelector(".mode").addEventListener("click", function(){
+  
   document.querySelector("body").classList.toggle("dark-mode");
+  
+  if(document.querySelector(".mode").innerText === "Dark Theme") {
+        document.querySelector(".mode").innerText = "White Theme";
+    }
+    else {
+        document.querySelector(".mode").innerText = "Dark Theme";
+    }
 });
